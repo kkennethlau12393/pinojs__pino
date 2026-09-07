b8edaecde2e0aa2846ab6133a43a449c2e45deaf
abloh-census <census@abloh.invalid>
2026-09-07 12:48:51 +0800

    Revert "fix(multistream): fix clone add crash and missing end/remove methods (#2493)"
    This reverts commit b394c2c16ac6a8919cd33d7f5684a01baca1096f.

--- pino.d.ts
+++ pino.d.ts
@@ -301,9 +301,7 @@ declare namespace pino {
     export interface MultiStreamRes<TOriginLevel = Level> {
       write: (data: any) => void,
       add: <TLevel = Level>(dest: StreamEntry<TLevel> | DestinationStream) => MultiStreamRes<TOriginLevel & TLevel>,
-      remove: (id: number) => MultiStreamRes<TOriginLevel>,
       flushSync: () => void,
-      end: () => void,
       minLevel: number,
       streams: StreamEntry<TOriginLevel>[],
       clone<const TLevel = Level>(level: TLevel): MultiStreamRes<TLevel>,
